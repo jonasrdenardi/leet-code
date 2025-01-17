e8343bde21a94628c7413d2ae40eede3350162d6
Jonas Denardi <jonas_denardi@hotmail.com>
2025-01-16 23:26:04 -0300

    Add _121_BestTimeToBuyAndSellStock e _130_CloneGraph

---
+++ _121_BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,18 @@
+﻿
+var result = MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+result = MaxProfit(new int[] { 7, 6, 4, 3, 1 });
+result = MaxProfit(new int[] { 2, 4, 1 });
+
+int MaxProfit(int[] prices)
+{
+    int minPrice = int.MaxValue;
+    int maxProfit = 0;
+
+    for (var i = 0; i < prices.Length; i++)
+    {
+        minPrice = Math.Min(prices[i], minPrice);
+        maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
+    }
+
+    return maxProfit;
+}
---
+++ _80_RemoveDuplicatesFromSortedArrayII/Program.cs
@@ -0,0 +1,26 @@
+﻿var nums = new int[] { 1, 1, 1, 2, 2, 3 };
+
+var result = RemoveDuplicates(nums);
+
+Console.WriteLine(result);
+
+static int RemoveDuplicates(int[] nums)
+{
+    if (nums.Length <= 2) return nums.Length;
+
+    // Começa na terceira posição pois é o maximo permitido em duplicidade.
+    int slow = 2;
+
+    for (int fast = 2; fast < nums.Length; fast++)
+    {
+        // Se for igual quer dizer que está repetido, então o ponteiro do fast vai percorrer até encontrar um diferente e trazer para o substituir a possição repetida.
+        // Assim  ponteiro slow pode ir para frente.
+        if (nums[fast] != nums[slow - 2])
+        {
+            nums[slow] = nums[fast];
+            slow++;
+        }
+    }
+
+    return slow;
+}
