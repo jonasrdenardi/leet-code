c22ea2b7e650abc6d23b55f9c7cbfa1ac150b80e
Jonas Denardi <jonas_denardi@hotmail.com>
2025-07-20 20:22:23 -0300

    Add _206_ReverseLinkedList

---
+++ _206_ReverseLinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using _206_ReverseLinkedList;
+
+// head = [1,2,3,4,5]
+var node5 = new ListNode(5);
+var node4 = new ListNode(4, node5);
+var node3 = new ListNode(3, node4);
+var node2 = new ListNode(2, node3);
+var node1 = new ListNode(1, node2);
+
+var resp = ReverseList(node1);
+Console.WriteLine(resp);
+
+ListNode ReverseList(ListNode head)
+{
+    ListNode prev = null;
+    ListNode current = head;
+
+    while (current != null)
+    {
+        var next = current.next; // save the next
+        current.next = prev; // invert the pointer
+        prev = current; // advance prev
+        current = next; // advance next
+    }
+
+    return prev;
+}
