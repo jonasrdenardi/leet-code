cda882600198696222a37e90af57eef758d14322
Jonas Denardi <jonas_denardi@hotmail.com>
2025-07-22 22:02:04 -0300

    Add _20_ValidParentheses

---
+++ _20_ValidParentheses/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+var resp = IsValid("(]");
+Console.WriteLine(resp);
+
+bool IsValid(string s)
+{
+    var stack = new Stack();
+
+    for (var i = 0; i < s.Length; i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            stack.Push(s[i]);
+            continue;
+        }
+        else
+        {
+            if (stack.Count == 0)
+                return false;
+
+            var item = (char)stack.Peek();
+            if ((s[i] == ')' && item != '(') ||
+                (s[i] == '}' && item != '{') ||
+                (s[i] == ']' && item != '['))
+                return false;
+
+            stack.Pop();
+        }
+    }
+
+    return stack.Count == 0;
+}
